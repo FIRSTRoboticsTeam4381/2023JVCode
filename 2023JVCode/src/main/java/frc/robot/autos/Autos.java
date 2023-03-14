3c82e6959a033e10a5c917ab31f45597f9495e27
THatMustacheCat <shadowdemon2114@gmail.com>
2023-03-14 19:30:09 -0400

    2 cube auto that has succeeded multiple times in which each of the runs were 15 seconds

--- 2023JVCode/src/main/java/frc/robot/autos/Autos.java
+++ 2023JVCode/src/main/java/frc/robot/autos/Autos.java
@@ -36,6 +36,12 @@ public final class Autos {
             new WaitCommand(0.5),
             RobotContainer.Winch.goToPosition(10000, 200) // ERROR is the RANGE -- THis is for John because he cant remember what it is.
         )),
+        Map.entry("placeMid", new SequentialCommandGroup(
+            SpecialistPositions.midPlacement(),
+            RobotContainer.Gripper.cubeGripper(),
+            new WaitCommand(0.5),
+            RobotContainer.Winch.goToPosition(10000, 200) // ERROR is the RANGE -- THis is for John because he cant remember what it is.
+        )),
         Map.entry("zero", SpecialistPositions.zero()),
         Map.entry("balance", RobotContainer.balanceRobot),
         Map.entry("lowerArm", RobotContainer.Winch.goToPosition(10010, 200)),
